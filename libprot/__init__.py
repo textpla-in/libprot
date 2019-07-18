bc295b27d5b31d35e0c18bb37c8ea657025c4144
Nate Guerin <nate@textpla.in>
2019-07-18 15:58:40 -0400

    add windows and linux reduces

--- libprot/__init__.py
+++ libprot/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from pathlib import Path
+from platform import system
 
 """Top-level package for libprot."""
 from prody import confProDy
@@ -11,5 +12,10 @@ __version__ = '0.1.3'
 
 confProDy(verbosity='none')
 
-REDUCE_BIN = Path(os.path.dirname(__file__), 'bin', 'reduce', 'reduce')
+REDUCE_BIN = Path(os.path.dirname(__file__), 'bin', 'reduce', system(), 'reduce')
 REDUCE_DB = Path(os.path.dirname(__file__), 'bin', 'reduce', 'reduce_wwPDB_het_dict.txt')
+
+if not REDUCE_BIN.exists():
+    raise Exception(f'Platform {system()} is not supported. `reduce` was not found at {REDUCE_BIN.absolute()}')
+REDUCE_BIN = str(REDUCE_BIN.absolute())
+REDUCE_DB = str(REDUCE_DB.absolute())
