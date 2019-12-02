6dc2b11a04061b06b0d00a6c2b8d8cf2ffa3579b
textpla-in <58409122+textpla-in@users.noreply.github.com>
2019-12-01 22:06:01 -0500

    Update misc.py

--- libprot/misc.py
+++ libprot/misc.py
@@ -1,2 +1,3 @@
 def safeify_id(record_id: str):
+    """Converts all non-alphanumeric characters in `record_id` into hyphens, and returns the string."""
     return ''.join(c if c.isalnum() else '-' for c in record_id)
